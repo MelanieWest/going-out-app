8c68dfabf57d1092f65ac98964533dfb1d57bded
Melanie West <dhmofan@gmail.com>
2017-09-18 17:20:25 -0400

    Attempt to add table with results.  Currently have error 404 so I can't find address and table attempt is commmented out

--- assets/javascript/groupon.js
+++ assets/javascript/groupon.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 // 'activity' and 'zip' will be passed in;  for now they are not so I am setting default values
 
 var activity = 'r';
-var zip = 32809;
+var zip = '32809';
 
 //these will be determined by input
 
@@ -24,7 +24,7 @@ $.ajax({
 }).done(function(response){
     console.log(response);
     console.log(response.city);
-    city = response.city
+    city = response.city;
 });  // end of zip to city ajax request;
 
 
@@ -33,14 +33,26 @@ $.ajax({
         method:  "GET"
     }).done(function(e){
 
-
         console.log(e);
 
+        //beginning attempts to send data to document in table form.  Currently not
+        //seeing data to find address
+
+        // $("#display").append('<table style="width:100%>');
+        // $("#display").append('<tr><td>"activity"</td>');
+        // $("#display").append('<td>"date"</td></tr>');
+        
         for (var i = 0; i < e.deals.length; i++){
             console.log("Activity number " + i+ " is " + e.deals[i].id);
-        }
- 
 
+        //     $("#display").append('<tr><td>'+e.deals[i].id+'</td>');
+        //     $("#display").append('<td>'+e.deals[i].id+'</td></tr>');
+
+         }      //end of for loop
+
+        // $("#display").append('</table>');
+        
     });  //end of function
+
 
 })      //end of document ready
