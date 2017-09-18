a160c4acee42574aff8bb4fb159b630d6d206b09
Melanie West <dhmofan@gmail.com>
2017-09-18 18:00:26 -0400

    Output table was formed incorrectly.  Clean out and start over

--- assets/javascript/groupon.js
+++ assets/javascript/groupon.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 // 'activity' and 'zip' will be passed in;  for now they are not so I am setting default values
 
 var activity = 'r';
-var zip = '32809';
+var zip = 32809;
+var tableDiv;
 
 //these will be determined by input
 
@@ -18,7 +19,7 @@ if (activity == 'activ'){category = 'things-to-do';}
 
 var zipUrl = "https://www.zipcodeapi.com/rest/Kw7CrOFpJDGmp331IVUUOY7Pue98LjOwQW9hLPGutnQSqTT8PO3DMbshpCpbWeIM/info.json/" + zip + "/miles"
 
-$.ajax({
+$.ajax({        // this request is listed first, but it logs second (after groupon)
     url: zipUrl,
     method:  "GET"
 }).done(function(response){
@@ -27,31 +28,21 @@ $.ajax({
     city = response.city;
 });  // end of zip to city ajax request;
 
+    tableDiv = $('<table>');
 
     $.ajax({
-        url: "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&division_id="+city+"&filters=category:"+category+"&offset=0&limit=10",
+        url: "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&division_id=Orlando&filters=category:"+category+"&offset=0&limit=10",
         method:  "GET"
     }).done(function(e){
 
         console.log(e);
-
-        //beginning attempts to send data to document in table form.  Currently not
-        //seeing data to find address
-
-        // $("#display").append('<table style="width:100%>');
-        // $("#display").append('<tr><td>"activity"</td>');
-        // $("#display").append('<td>"date"</td></tr>');
         
         for (var i = 0; i < e.deals.length; i++){
             console.log("Activity number " + i+ " is " + e.deals[i].id);
 
-        //     $("#display").append('<tr><td>'+e.deals[i].id+'</td>');
-        //     $("#display").append('<td>'+e.deals[i].id+'</td></tr>');
-
          }      //end of for loop
 
-        // $("#display").append('</table>');
-        
+x        
     });  //end of function
 
 
