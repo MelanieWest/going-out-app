ab633fc69f029acc16a7aea8b525f2e08eeb5f1b
Julia McGee <JuliaMcGee@Kenneths-MacBook-Pro.local>
2017-09-19 17:22:28 -0400

    testing categories without luck for events. Need to change ajax call to find categories

--- assets/javascript/meetup.js
+++ assets/javascript/meetup.js
@@ -1,33 +1,44 @@
 $(document).ready(function(){
-var zip = 32792;
-var zipUrl = "https://www.zipcodeapi.com/rest/Kw7CrOFpJDGmp331IVUUOY7Pue98LjOwQW9hLPGutnQSqTT8PO3DMbshpCpbWeIM/info.json/" + zip + "/degrees"
+// var zip = 32792;
+// var zipUrl = "https://www.zipcodeapi.com/rest/Kw7CrOFpJDGmp331IVUUOY7Pue98LjOwQW9hLPGutnQSqTT8PO3DMbshpCpbWeIM/info.json/" + zip + "/degrees"
 
-//on-click event wouldn't work when I used button id, but the click event does work when clicking on the form; just added this here for testing purposes
-$("form").on("click", function() {
-$.ajax({      
-    url: zipUrl,
-    method:  "GET"
-}).done(function(response){
-    console.log(response);
-    var lat = response.lat
-    var lng = response.lng
-    var queryUrl = "https://api.meetup.com/find/events?&only=name,time,venue,link,description&omit=time,venue.id,venue.country,venue.localized_country_name,venue.repinned,venue.lat,venue.lon&key=2fb727dc2e2c2b62374217f5d45a&sign=true&photo-host=public&lon=" + lng + "&lat=" + lat 
+// //on-click event wouldn't work when I used button id, but the click event does work when clicking on the form; just added this here for testing purposes
+// $("form").on("click", function() {
+// $.ajax({      
+//     url: zipUrl,
+//     method:  "GET"
+// }).done(function(response){
+//     console.log(response);
+//     var lat = response.lat
+//     var lng = response.lng
+//     var queryUrl = "https://api.meetup.com/find/topics?&query=tech&key=2fb727dc2e2c2b62374217f5d45a&sign=true&photo-host=public&lon=" + lng + "&lat=" + lat 
+//     $.ajax({
+//     url: queryUrl,
+//     method: "GET"
+//     }).done(function(responseMeetup) {
+//     for(var i = 0; i < responseMeetup.length; i++) {
+//     console.log(responseMeetup);
+//     var name = responseMeetup[i].name;
+//     var link = responseMeetup[i].link;
+//     var str = "Meetup Link";
+//     var result = str.link(link); 
+//     $("#display").html("Event Name: " + name + " " + "Link: " + result);
+//     };
+// }); 
+// });
+// });
+
+
+
+    var queryUrl = "https://api.meetup.com/find/groups?&key=2fb727dc2e2c2b62374217f5d45a&sign=true&photo-host=public"
     $.ajax({
     url: queryUrl,
     method: "GET"
     }).done(function(responseMeetup) {
-    for(var i = 0; i < responseMeetup.length; i++) {
     console.log(responseMeetup);
-    var name = responseMeetup[i].name;
-    var link = responseMeetup[i].link;
-    var str = "Meetup Link";
-    var result = str.link(link); 
-    $("#display").html("Event Name: " + name + " " + "Link: " + result);
-    };
 }); 
 });
-});
-});
+        
 
 
 
