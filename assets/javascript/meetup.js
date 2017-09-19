bae3a7abf500632eb2c4009471b39b1ee81b9de6
Julia McGee <JuliaMcGee@Kenneths-MacBook-Pro.local>
2017-09-18 21:31:13 -0400

    changes3

--- assets/javascript/meetup.js
+++ assets/javascript/meetup.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-var queryUrl = "https://api.meetup.com/find/events&key=2fb727dc2e2c2b62374217f5d45a&sign=true&photo-host=public"
+var queryUrl = "https://api.meetup.com/find/events?&only=name,time,venue&key=2fb727dc2e2c2b62374217f5d45a&sign=true&photo-host=public"
 $.ajax({
 url: queryUrl,
 method: "GET"
