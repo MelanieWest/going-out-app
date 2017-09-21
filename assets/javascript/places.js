52035867d6579cd2524656cdd24bf60d1ae0dadb
Melanie West <dhmofan@gmail.com>
2017-09-21 01:36:19 -0400

    Add console log messages to see where I'm getting and test values

--- assets/javascript/places.js
+++ assets/javascript/places.js
@@ -41,7 +41,8 @@ function places () {
       function initialize() {
 
       var city = new google.maps.LatLng(lat,lng);
-        
+        console.log ('city is '+city);
+
       map = new google.maps.Map(document.getElementById('map'), {
           center: city,
           zoom: 15      //zoom: 0 shows the whole earth.  Bigger #'s are closer
@@ -67,6 +68,8 @@ function places () {
       
       itemSel = Math.floor(Math.random()*results.length);
 
+      console.log('results.length is ' + results.length);
+      
       if (results.length == 0){ 
         tableDiv.append('<p> No results found in the category: '+theme+ '</p>');
         tableDiv.append('<p> Please try again (a new category will be selected) </p> ')
