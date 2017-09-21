9e0f95975a6157d5c816f69540e830256180cbd3
Melanie West <dhmofan@gmail.com>
2017-09-20 23:45:15 -0400

    Added a return inside the edgecase if statement.  All else the same.

--- assets/javascript/places.js
+++ assets/javascript/places.js
@@ -1,6 +1,6 @@
 function places () {
 
-$("#display").append('<div id ="map">Map</div>');
+$("#display").append('<div id ="map">Google Maps</div>');
 
     // for now, the only input received by this function is 'zip' (zip code) - global
   
@@ -70,7 +70,8 @@ $("#display").append('<div id ="map">Map</div>');
       if (results.length == 0){ 
         tableDiv.append('<p> No results found in the category: '+theme+ '</p>');
         tableDiv.append('<p> Please try again (a new category will be selected) </p> ')
-        $("#display").prepend(tableDiv); //insert table into document       
+        $("#display").prepend(tableDiv); //insert table into document
+        return       
       }
 
       if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -98,7 +99,6 @@ $("#display").append('<div id ="map">Map</div>');
       
     }           // end of callback function
   
-    
-    });   // end of zip
+  });   // end of zip
    
 }   // end of function 'places' block
